b60e4fdccd8f9ab0506554ad50696edac63c5cb3
lindblomdavid <daavidlindblom@gmail.com>
2023-04-27 16:13:47 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import About from './components/AboutPage/About';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
