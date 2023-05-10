5c889bf1c8c97f1ac5ac19e4d1153da5e9c694d4
lindblomdavid <daavidlindblom@gmail.com>
2023-05-10 08:17:24 +0200

    added transition effect to pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
+// App.js
 import React from 'react';
-import ChatComponent from './components/ChatComponent/ChatComponent';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  useRoutes,
+  useLocation,
+} from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 import About from './components/AboutPage/About';
 import Policies from './components/Policies/Policies';
 import './App.css';
 import NavbarComponent from './components/Navbar/Navbar';
-
-// Import your components for each route here
-// import Riktlinjer from './components/Riktlinjer';
-// import UserCase from './components/UserCase';
-// import Login from './components/Login';
+import ChatComponent from './components/ChatComponent/ChatComponent';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <NavbarComponent />
-        <main>
-          <Routes>
-            <Route path="/" element={<ChatComponent />} />
-            <Route
-              path="/om"
-              element={
-                <div>
-                  <About />
-                </div>
-              }
-            />
-
-            <Route path="/chatt" element={<ChatComponent />} />
-            <Route path="/riktlinjer" element={<Policies />} />
-          </Routes>
-        </main>
-      </div>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: { opacity: 0, filter: 'blur(10px)' },
+    enter: { opacity: 1, filter: 'blur(0px)' },
+    leave: { opacity: 0, filter: 'blur(10px)' },
+    config: { duration: 1000 },
+  });
+  // Define your routes using useRoutes
+  const routes = useRoutes([
+    { path: '/', element: <ChatComponent /> },
+    { path: '/om', element: <About /> },
+    { path: '/chatt', element: <ChatComponent /> },
+    { path: '/riktlinjer', element: <Policies /> },
+  ]);
+
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <main style={{ position: 'relative' }}>
+        {transitions((props, item) => (
+          <animated.div
+            style={{
+              ...props,
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+            }}
+          >
+            {routes}
+          </animated.div>
+        ))}
+      </main>
+    </div>
   );
 }
 
